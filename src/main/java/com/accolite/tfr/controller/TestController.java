ba7884061f2c71b7b6ddf6a6fd31fd83331ce944
Mokara Vandana <mokara.v@accolite.com>
2022-12-28 11:04:26 +0530

    DTO changes done

--- src/main/java/com/accolite/tfr/controller/TestController.java
+++ src/main/java/com/accolite/tfr/controller/TestController.java
@@ -1,15 +1,11 @@
 package com.accolite.tfr.controller;
 
-import com.accolite.tfr.dto.FeatureDto;
 import com.accolite.tfr.dto.OrganisationDto;
 import com.accolite.tfr.dto.ProjectDto;
-import com.accolite.tfr.model.Feature;
 import com.accolite.tfr.model.Organisation;
 import com.accolite.tfr.model.Project;
 import com.accolite.tfr.service.OrganisationServiceImp;
 import com.accolite.tfr.service.ProjectServiceImp;
-//import com.accolite.tfr.service.Service;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,32 +28,8 @@ public class TestController {
     @Autowired
     private OrganisationServiceImp organisationService;
 
-//    @Autowired
-//    private Service service;
-//    @PostMapping("/project")
-//    private ResponseEntity<Project> addProject(@RequestBody Project project ){
-//        return ResponseEntity.ok().body(this.service.addProject(project));
-//    }
-//    @GetMapping("/organisation/{org_id}")
-//    private ResponseEntity<Organisation> organisation(@PathVariable int org_Id){
-//        return ResponseEntity.ok().body(this.service.getOrganisation(org_Id));
-//    }
-//    @GetMapping("/feature/{feature_id}")
-//    private ResponseEntity<Feature> organisation(@PathVariable int feature_id){
-//        return ResponseEntity.ok().body(this.service.getFeature(feature_id));
-//    }
-//    @PostMapping("/feature")
-//    private  ResponseEntity<FeatureDto> addFeature(@RequestBody Feature feature){
-//        Feature newFeature = this.service.addFeature(feature);
-//        FeatureDto featureDto = new FeatureDto();
-//        BeanUtils.copyProperties(newFeature,featureDto);
-//        return ResponseEntity.ok().body(featureDto);
-    //}
     @PostMapping("/project")
     private  ResponseEntity<ProjectDto> addProject(@RequestBody ProjectDto projectDto){
-//        Project newProject = this.service.addProject(project);
-//        ProjectDto projectDto = new ProjectDto();
-//        BeanUtils.copyProperties(newProject,projectDto);
         Project org = this.projectService.addProject(projectDto);
         ProjectDto pdto = this.projectService.mapToDto(org);
         return ResponseEntity.ok().body(pdto);
@@ -65,11 +37,6 @@ public class TestController {
 
     @PostMapping("/addDivision")
     private ResponseEntity<OrganisationDto> addDivision(@RequestBody OrganisationDto organisation){
-//        Organisation newOrganisation = this.service.addOrganisation(organisation);
-//        OrganisationDto organisationDto = new OrganisationDto(organisation);
-        //BeanUtils.copyProperties(newOrganisation,organisationDto);
-        //OrganisationDto od = service.mapToDto(organisation);
-        //Organisation org = service.mapToEntity(organisation);
         Organisation org = this.organisationService.addOrganisation(organisation);
         OrganisationDto od = this.organisationService.mapToDto(org);
         return ResponseEntity.ok().body(od);
