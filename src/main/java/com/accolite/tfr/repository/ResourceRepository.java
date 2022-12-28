c52129773217b78af1c364a4361b789635ffa497
ayushiShahare <ayushi.shahare@accolitedigital.com>
2022-12-28 12:51:52 +0530

    correcting

--- src/main/java/com/accolite/tfr/repository/ResourceRepository.java
+++ src/main/java/com/accolite/tfr/repository/ResourceRepository.java
@@ -1,6 +1,5 @@
 package com.accolite.tfr.repository;
 
-import com.accolite.tfr.model.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResourceRepository extends JpaRepository<Resource,Integer> {
