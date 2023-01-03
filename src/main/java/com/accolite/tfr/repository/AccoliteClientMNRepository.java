1ac4f9821609e56a69eb3a9793ddb341465e00f5
mokara.v <mokara.vandana@accolitedigital.com>
2023-01-03 16:36:28 +0530

    DTO with services and controller working

--- src/main/java/com/accolite/tfr/repository/AccoliteClientMNRepository.java
+++
@@ -1,6 +0,0 @@
-package com.accolite.tfr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccoliteClientMNRepository extends JpaRepository<AccoliteClientMN,Integer> {
-}
--- src/main/java/com/accolite/tfr/service/AccoliteClientMNService.java
+++
@@ -1,7 +0,0 @@
-package com.accolite.tfr.service;
-
-public interface AccoliteClientMNService {
-    public AccoliteClientMN addAccoliteClientMN(AccoliteClientMNModel accoliteClientMNModel);
-    public AccoliteClientMNModel mapToDto(AccoliteClientMN org);
-    public AccoliteClientMN mapToEntity(AccoliteClientMNModel dto);
-}
