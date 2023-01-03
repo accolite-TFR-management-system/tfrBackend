f3c32eaf83b53b539001e7707fca30e2dbac0409
mokara.v <mokara.vandana@accolitedigital.com>
2023-01-03 15:59:19 +0530

    DTO with services and controller working

--- src/main/java/com/accolite/tfr/controller/ClientsController.java
+++ src/main/java/com/accolite/tfr/controller/ClientsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tfr")
+@CrossOrigin(origins="http://localhost:4200")
 public class ClientsController {
 
     @Autowired
--- src/main/java/com/accolite/tfr/controller/ResourceHistoryController.java
+++ src/main/java/com/accolite/tfr/controller/ResourceHistoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tfr")
+@CrossOrigin(origins="http://localhost:4200")
 public class ResourceHistoryController {
 
     @Autowired
